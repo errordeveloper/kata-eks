a3ef109754c372323f47420f4d1181ed964d9709
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-04-09 14:46:21 +0100

    Add new readiness check script for master, extract image preload script into separate file

--- images/kubeadm-ubuntu/configure.sh
+++ images/kubeadm-ubuntu/configure.sh
@@ -179,36 +179,6 @@ oom_score = 0
     base_image_size = ""
 EOF
 
-cat > /usr/bin/preload-images.sh << EOF
-#!/bin/bash
-
-set -o errexit
-set -o pipefail
-set -o nounset
-
-
-if ! [ -d /images ] ; then
-  echo "no /images directory present, skip image preload"
-  exit 0
-fi
-
-# TODO pick master/node images also
-dirs=(
-  "/images/container/common"
-  "/images/container/common_\${KUBERNETES_VERSION}"
-  "/images/container/control_plane_\${KUBERNETES_VERSION}"
-)
-images=(\$(find "\${dirs[@]}" -name image.tar))
-
-if [ -z "\${images+x}" ] ; then
-  echo  "no images found in \${dirs[@]}, skip image preload"
-  exit 0
-fi
-
-echo "\${images[@]}" | xargs --max-procs=2 --max-args=1 ctr -n k8s.io images import
-EOF
-chmod +x /usr/bin/preload-images.sh
-
 cat > /etc/systemd/system/images.service << EOF
 [Unit]
 After=containerd.service
---
+++ images/kubeadm-ubuntu/is-master-ready.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+export KUBECONFIG="/etc/kubernetes/admin.conf"
+
+if ! systemctl --no-pager is-active kubeadm.target > /dev/null ; then
+  echo "systemd kubeadm.target is not yet active"
+  exit 1
+fi
+
+if ! [ -e "${KUBECONFIG}" ] ; then
+  echo "${KUBECONFIG} has not been written yet"
+  exit 1
+fi
+
+node_name="$(hostname)"
+
+jsonpath='{@.status.conditions[?(@.type=="Ready")].status}'
+ready="$(kubectl get node "${node_name}" --output "jsonpath=${jsonpath}" || printf Error)"
+
+if ! [ "${ready}" = "True" ] ; then
+  echo "${node_name} not ready yet"
+  exit 1
+fi
+echo "${node_name} is ready now"
+exit 0
