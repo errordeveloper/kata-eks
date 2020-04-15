7acc74e478d8644878f70cac4e7a724b672c6eee
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-04-15 13:46:04 +0100

    Prevent kubeadm from talking to the to parent cluster, fix var name

--- images/kubeadm-ubuntu/write-secrets.sh
+++ images/kubeadm-ubuntu/write-secrets.sh
@@ -6,12 +6,13 @@ set -o nounset
 
 cluster="$(sed -n 's/^cluster="\(.*\)"$/\1/p' "/etc/kubeadm/metadata/labels")"
 
-export KUBECONFIG="/etc/parent-management-cluster/kubeconfig"
-
-join_token="$(kubeadm token create --ttl=0 --description="Secondary token for automation")"
+join_token="$(kubeadm token create --ttl=0 --description="Secondary token for automation" --v=9)"
 ca_hash="$(sha256sum /etc/kubernetes/pki/ca.crt)"
 
-join_token_js="$(printf '{"stringData":{"token": "%s", "ca_hash": "%s"}}' )" "${join_token}" "sha256:${hash%% *}"
+# kubeadm token create actually respects KUBECONFIG, that is undesired, so we set it here
+export KUBECONFIG="/etc/parent-management-cluster/kubeconfig"
+
+join_token_js="$(printf '{"stringData":{"token": "%s", "ca_hash": "%s"}}' )" "${join_token}" "sha256:${ca_hash%% *}"
 
 kubectl patch secret "${cluster}-join-token" --patch="${join_token_js}"
 
