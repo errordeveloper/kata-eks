4c26711ef92c75861569e3d31d57b8fd86b0bf72
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-04-21 14:59:05 +0100

    Update syntax for latest kubectl

--- kubectl-each-master.sh
+++ kubectl-each-master.sh
@@ -7,5 +7,5 @@ set -o nounset
 ready_pods=($(kubectl get pods -l role=master -o json | jq -r '.items[] | select(.status.phase == "Running") | .metadata.name'))
 
 for pod in "${ready_pods[@]}" ; do
-  kubectl exec "${pod}" kubectl -- "$@"
+  kubectl exec "${pod}" -- kubectl "$@"
 done
